9e23707e3119061be95d400fb4f68899026ee762
ombrellus <51714864+ombrellus@users.noreply.github.com>
2024-03-26 18:25:05 +0100

    Delete ombrellus-modutils directory

--- ombrellus-modutils/extensions/src/enemy/enemy.gd
+++
@@ -1,9 +0,0 @@
-extends "res://src/enemy/enemy.gd"
-
-@onready var utils = get_node("/root/ModLoader/ombrellus-modutils")
-
-func hit(body):
-	super.hit(body)
-	if body.is_in_group("bullet") or body.is_in_group("finger") or body.is_in_group("laser") or body.is_in_group("bullet_splash") or body.is_in_group("slash"):
-		utils.enemyHit.emit(parent, body)
-		
--- ombrellus-modutils/extensions/src/ui/shop/shop.gd
+++
@@ -1,7 +0,0 @@
-extends "res://src/ui/shop/shop.gd"
-
-@onready var utils = get_node("/root/ModLoader/ombrellus-modutils")
-
-func _ready():
-	super._ready()
-	shopItemChoices.merge(utils.customUpgrades)
