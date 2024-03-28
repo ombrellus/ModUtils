29910445a0ed08f27a09aacda74d0ae282e48a5f
ombrellus <51714864+ombrellus@users.noreply.github.com>
2024-03-28 11:44:33 +0100

    Delete mods-unpacked/torcado-nametag directory

--- mods-unpacked/torcado-nametag/extensions/src/player/player.gd
+++
@@ -1,9 +0,0 @@
-# Our base script is the original game script.
-extends "res://src/player/player.gd"
-
-# This overrides the method with the same name, changing the value of its argument:
-func _ready():
-	super._ready()
-	
-	print_debug("i'm the player!")
-	Utils.place(preload("res://mods-unpacked/torcado-nametag/extensions/src/player/nametag.tscn"), self)
--- mods-unpacked/torcado-nametag/global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal apply_config(config:ModConfig)
-signal disable()
-signal nametag_created(node:Node)
-
-var nametag_label:Label
