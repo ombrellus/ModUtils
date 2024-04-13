1a94923e5118e5a20fe25a4f13a49a50e6588fe1
ombrellus <51714864+ombrellus@users.noreply.github.com>
2024-04-13 21:29:53 +0200

    examples character

---
+++ examples/Custom character/yourname-modname/extensions/src/character/cool_character/cool_character.gd
@@ -0,0 +1,85 @@
+extends Node2D
+
+var root:Node2D
+@onready var canvas = $canvas
+@onready var flash_canvas = $flashCanvas
+@onready var char_bg = $canvas/charBG
+@onready var char_fg = $canvas/charFG
+@onready var char_flash_bg = $flashCanvas/charFlashBG
+@onready var char_flash_fg = $flashCanvas/charFlashFG
+
+@onready var Bullet = preload("res://src/element/bullet/bullet.tscn")
+
+@onready var mod = get_node("/root/ModLoader/yourname-modname")
+
+var shootTimer = 0.0
+
+func init():
+	pass
+
+func _ready():
+	#canvas.draw.connect(draw)
+	#flash_canvas.draw.connect(draw)
+	updateChar()
+
+func updateChar():
+	#sprite.modulate = root.color
+	#char_bg.modulate = root.bgColor
+	#canvas.queue_redraw()
+	#flash_canvas.queue_redraw()
+	pass
+
+func _process(delta):
+	#Abilty cooldown needs to me manually changed if you have a modded usable ability
+	if not Global.escaped:
+		if Global.abilityCooldown > 0.0:
+			var speed = 1.0/(25.0 - TorCurve.smoothCorner(max(1, mod.coolAbility), 19.0, 1.8, 2.8)) * delta
+			Global.abilityCooldown -= speed
+
+func updateAttack(delta):
+	shootTimer -= 1.0 * delta
+	if shootTimer <= 0.0 and root.shooting > 0.0:
+		#shootTimer = 1.0 / (0.36 * (Global.fireRate - 1.0 * Global.multiShot) + (1.0 / 0.4))
+		#shootTimer = 1.0 / (0.36 * (Global.fireRate) + (1.0 / 0.4))
+		if Global.torrentActive:
+			shootTimer = 0.2 - 0.1 * TorCurve.smoothCorner(Global.fireRate/15.0, 1.0, 1.0, 4.0)
+		else:
+			shootTimer = 0.95 / (0.3 * pow(Global.fireRate, 1.15) + (1.0 / 0.4))
+			shootTimer *= 1.0 + (0.33 * pow(Global.multiShot + Global.multiShotExtra, 0.75))
+		
+		var count := float(Global.multiShot + Global.multiShotExtra) + 1.0
+		var spread = min(TAU, (0.8*(count-0.66)) * TAU / 32.0)
+		
+		Audio.play(preload("res://src/sounds/shoot2.ogg"), 0.8, 1.2)
+		for i in count:
+			var a = 0.0
+			if count > 1:
+				a = ((i) / (count - 1.0)) - 0.5
+			var angle2 = root.aimAngle.rotated(a * spread)
+			Utils.spawn(Bullet, root.position, root.get_parent(), {angle = angle2})
+			Stats.stats.totalBulletsFired += 1
+			Stats.metaStats.totalBulletsFired += 1
+		
+
+func updateMove(delta):
+	root.velocity = Utils.lexp(root.velocity, root.moveDir * root.max_speed, 20.0 * delta)
+	
+
+func useAbility(delta):
+	if mod.coolAbility > 0:
+		if Global.escaped:
+			return
+		if (Global.abilityCooldown > 0.0 and Global.refreshStack <= 0):
+			return
+		var time:float = 140.0
+		Global.abilityTimer = time
+		Global.abilityCooldown = 1.0
+		# a non existing ability
+		Global.curAbility = 7
+		Global.main.useAbility(7)
+
+#func draw():
+	#if not (Global.save.specialFace and Global.options.showFace):
+		#canvas.draw_circle(Vector2.ZERO, 16.0, root.bgColor)
+		#canvas.draw_arc(Vector2.ZERO, 16.0, 0.0, TAU, 30, Color.BLACK, 10.0, true)
+		#canvas.draw_arc(Vector2.ZERO, 16.0, 0.0, TAU, 30, root.color, 3.3, true)
---
+++ examples/Custom character/yourname-modname/mod_main.gd
@@ -0,0 +1,78 @@
+extends Node
+
+const AUTHORNAME_MODNAME_DIR := "yourname-modname" # Name of the directory that this file is in
+const AUTHORNAME_MODNAME_LOG_NAME := "yourname-modname:Main" # Full ID of the mod (AuthorName-ModName)
+
+var mod_dir_path := ""
+var extensions_dir_path := ""
+var translations_dir_path := ""
+
+@onready var modUtils = get_node("/root/ModLoader/ombrellus-modutils")
+
+var coolAbilityCount:int = 0
+
+var myCharacter:Dictionary ={
+		internalName = "cool_character",
+		displayName = "Cool",
+		wallShrinkSpeed = 1,
+		wallResistance = 1,
+		priceScale = 1.0,
+		# leave blank
+		skins = [""],
+		baseWealth = 0,
+		spritesExtension = ".svg",
+		unlocked = true,
+	}
+var charAbility:Dictionary={
+		internalName = "cool",
+		name = (func()->Array:
+			return ["Cool ability"]
+			),
+		icon = (func()->Array:
+			return [load("res://src/player/bubbleBig.png")]
+			),
+		priceType = 1,
+		value = (func():
+			return coolAbilityCount
+			),
+		price = (func(): 
+			return (coolAbilityCount+1)
+			),
+		buy = (func():
+			coolAbilityCount += 1
+			Global.abilityCount += 1
+			Global.usableAbilityCount += 1
+			),
+		description = (func():
+			var list = [
+				"1",
+				"2",
+				"3"
+			]
+			if coolAbilityCount < list.size():
+				return list[coolAbilityCount]
+			return ""
+			),
+		weight = 1.0,
+		_weight = 1.0
+	}
+
+# ! your _ready func.
+func _init() -> void:
+	ModLoaderLog.info("Init", AUTHORNAME_MODNAME_LOG_NAME)
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(AUTHORNAME_MODNAME_DIR)
+	install_script_extensions()
+
+
+func install_script_extensions() -> void:
+	# ! any script extensions should go in this directory, and should follow the same directory structure as vanilla
+	extensions_dir_path = mod_dir_path.path_join("extensions")
+
+
+func _ready() -> void:
+	ModLoaderLog.info("Ready", AUTHORNAME_MODNAME_LOG_NAME)
+	modUtils.addCustomCharacter(AUTHORNAME_MODNAME_DIR,myCharacter,charAbility)
+	Events.runStarted.connect(func():
+		coolAbilityCount = 0)
+
+
